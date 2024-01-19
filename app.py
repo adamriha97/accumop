d87c6c738fbd7af735f201455ee51b9ebb64a0fc
Adam Říha <adam.riha97@gmail.com>
2024-01-19 17:47:08 +0100

    map added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="AccuMop", page_icon="world_map", layout="wide")
+st.set_page_config(page_title="AccuMop", page_icon="world_map", layout="centered") # wide
 
 file_path = 'akumulace_data.csv'
 df = pd.read_csv(file_path, encoding='Windows-1252', sep=';')
@@ -14,8 +14,13 @@ KRAJ_NAZEV = st.sidebar.multiselect(
     default=df["KRAJ_NAZEV"].unique()
 )
 
+columns_to_check = ['LATITUDE', 'LONGITUDE']
+df = df.dropna(subset=columns_to_check)
+
 df_selection = df.query(
     "KRAJ_NAZEV == @KRAJ_NAZEV"
 )
+
+st.map(df_selection, size=1, use_container_width=False)
 
 st.dataframe(df_selection)
